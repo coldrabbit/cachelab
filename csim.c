31b78ac450cf93198b423cc332f0737a1500a3cc
songjie <1737766745@qq.com>
2021-01-07 17:00:46 +0800

    final version

--- csim.c
+++ csim.c
@@ -1,16 +1,191 @@
-#include "cachelab.h"
+/***
+ * editor: songjie
+ * time: 2020.1.4
+ * function: cache simulator
+ ***/
 
-int hit_count, miss_count, eviction_count;//需要输出的三个参数，分别为命中数、缺失数、清出数
+#include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <string.h>
+
+int hit_count = 0,
+    miss_count = 0,
+    eviction_count = 0;//arguments required to output
 
 typedef struct{
-    int valid;
-    int tag;
-    int time;
-}cache_line;
+    int h;
+    int v;
+    int s;
+    int E;
+    int b;
+    char t[100];
+}arguments;//the arguments input from order
 
+arguments args;//use global argument for arguments from order
+
+typedef struct{
+    int validBit;
+    int tag;
+    int LRU_counter;
+}cache_line, *cache_set, **cache;//cache's structure
+
+cache myCache = NULL;//use global argument for cache
+
+void initCache(){//initial cache
+    int S = 1 << args.s;//cache has S = 2^s cache lines
+
+    myCache = (cache)malloc(sizeof(cache_set)*S);
+    for(int i = 0; i < S; i++){//malloc for each cache line
+        myCache[i] = (cache_set)malloc(sizeof(cache_line)*args.E);
+        for(int j = 0; j < args.E; j++){
+            myCache[i][j].validBit = 0;
+            myCache[i][j].tag = -1;
+            myCache[i][j].LRU_counter = 0;
+        }
+    }
+}
+
+void usage(){//-h print the help info
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+           "Options:\n"
+           "  -h         Print this help message.\n"
+           "  -v         Optional verbose flag.\n"
+           "  -s <num>   Number of set index bits.\n"
+           "  -E <num>   Number of lines per set.\n"
+           "  -b <num>   Number of block offset bits.\n"
+           "  -t <file>  Trace file.\n\n"
+           "Examples:\n"
+           "  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+           "  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
+void parseArgs(int argc, char* argv[]){
+    args.h = 0;
+    args.v = 0;
+    int opt;
+
+    while((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1){//parse arguments from order
+        switch(opt){
+            case 'h':
+                args.h = 1;
+                usage();
+                break;
+            case 'v':
+                args.v = 1;
+                break;
+            case 's':
+                args.s = atoi(optarg);
+                break;
+            case 'E':
+                args.E = atoi(optarg);
+                break;
+            case 'b':
+                args.b = atoi(optarg);
+                break;
+            case 't':
+                strcpy(args.t, optarg);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void update(int address){//update contents in cache by LRU
+    int tag = address >> (args.b + args.s);
+    int index = (address >> args.b) & ((1 << args.s) - 1);
+    cache_set set = myCache[index];
+
+    int i;
+
+    for(i = 0; i < args.E; i++){
+        if(set[i].validBit == 1)
+            set[i].LRU_counter++;
+    }
+
+    for(i = 0; i < args.E; i++){//hit
+        if(set[i].validBit == 1 && set[i].tag == tag){
+            hit_count++;
+            set[i].LRU_counter = 0;
+            return;
+        }
+    }
+
+    miss_count++;//miss
+    for(i = 0; i < args.E; i++){//there is blank line
+        if(set[i].validBit == 0){
+            set[i].validBit = 1;
+            set[i].tag = tag;
+            set[i].LRU_counter = 0;
+            return;
+        }
+    }
+
+    eviction_count++;//there is no blank line, evict a cache line
+    int max_LRU_counter = set[0].LRU_counter;
+    int max_LRU_counter_index = 0;
+    for(i = 0; i < args.E; i++){
+        if(set[i].LRU_counter > max_LRU_counter){
+            max_LRU_counter = set[i].LRU_counter;
+            max_LRU_counter_index = i;
+        }
+    }
+    set[max_LRU_counter_index].LRU_counter = 0;
+    set[max_LRU_counter_index].tag = tag;
+    return;
+}
+
+void freeCache(){//free the space of cache malloced in the past
+    int S = 1 << args.s;//set numbers of cache
+
+    for(int i = 0; i < S; i++){
+        free(myCache[i]);
+    }
+    free(myCache);
+}
+
+void parseCache(){//parse the trace file and update the cache
+    char operation;
+    int address;
+    int size;//three arguments of memory traces' order
+
+    FILE *fp = fopen(args.t, "r");//open the trace file
+    if(fp == NULL){
+        printf("file open failed\n");
+        exit(-1);
+    }
+
+    while(fscanf(fp, " %c %x,%d", &operation, &address, &size) > 0){
+        switch(operation){//'I' denotes an instruction load, ignore it
+            case 'L'://data load
+                update(address);
+                break;
+            case 'M'://data modify(a data load followed by a data store)
+                update(address);
+            case 'S'://data store
+                update(address);
+                break;
+            default:
+                break;
+        }
+    }
+
+    fclose(fp);
+
+    freeCache();//free the space of cache
+}
+
+
+
+/*main*/
 int main(int argc, char* argv[])
 {
-    hit_count = miss_count = eviction_count = 0;
+    parseArgs(argc, argv);
+    initCache();
+    parseCache();
     printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
--- trans.c
+++ trans.c
@@ -6,7 +6,14 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
+
+/***
+ * editor: songjie
+ * time: 2020.1.7
+ * function: matrix transpose
+ ***/ 
+
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -22,6 +29,108 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M == 32){//32x32 matrix divides to 16  8x8 blocks
+        int i, j;//scan the matrix
+        int k;//scan the 8x8 blocks by line
+        int v0, v1, v2, v3, v4, v5, v6, v7;//deposit some block's one line in A
+        for(i = 0; i < N; i += 8){
+            for(j = 0; j < M; j += 8){
+                for(k = i; k < i + 8; k++){
+                    v0 = A[k][j];
+                    v1 = A[k][j + 1];
+                    v2 = A[k][j + 2];
+                    v3 = A[k][j + 3];
+                    v4 = A[k][j + 4];
+                    v5 = A[k][j + 5];
+                    v6 = A[k][j + 6];
+                    v7 = A[k][j + 7];
+
+                    B[j][k] = v0;
+                    B[j + 1][k] = v1;
+                    B[j + 2][k] = v2;
+                    B[j + 3][k] = v3;
+                    B[j + 4][k] = v4;
+                    B[j + 5][k] = v5;
+                    B[j + 6][k] = v6;
+                    B[j + 7][k] = v7;
+                }
+            }
+        }
+    }
+    else if(M == 64){//64x64 matrix
+        int i, j;
+        int k;
+        int v0, v1, v2, v3, v4, v5, v6, v7;
+        for(i = 0; i < N; i += 8){
+            for(j = 0; j < M; j += 8){
+                /*8x8 blocks divides to 4  4x4 blocks*/
+                for(k = i; k < i + 4; k++){//the A's top left block trans to B's top left, A's top right block trans to B's top right
+                    v0 = A[k][j];
+                    v1 = A[k][j + 1];
+                    v2 = A[k][j + 2];
+                    v3 = A[k][j + 3];
+                    v4 = A[k][j + 4];
+                    v5 = A[k][j + 5];
+                    v6 = A[k][j + 6];
+                    v7 = A[k][j + 7];
+
+                    B[j][k] = v0;
+                    B[j + 1][k] = v1;
+                    B[j + 2][k] = v2;
+                    B[j + 3][k] = v3;
+                    B[j][k + 4] = v4;
+                    B[j + 1][k + 4] = v5;
+                    B[j + 2][k + 4] = v6;
+                    B[j + 3][k + 4] = v7;
+                }
+
+                for(k = j; k < j + 4; k++){//the B's top right block trans to B's bottom left, the A's bottom left block trans to B's top right
+                    v0 = B[k][i + 4];
+                    v1 = B[k][i + 5];
+                    v2 = B[k][i + 6];
+                    v3 = B[k][i + 7];
+                    v4 = A[i + 4][k];
+                    v5 = A[i + 5][k];
+                    v6 = A[i + 6][k];
+                    v7 = A[i + 7][k];
+
+                    B[k][i + 4] = v4;
+                    B[k][i + 5] = v5;
+                    B[k][i + 6] = v6;
+                    B[k][i + 7] = v7;
+                    B[k + 4][i] = v0;
+                    B[k + 4][i + 1] = v1;
+                    B[k + 4][i + 2] = v2;
+                    B[k + 4][i + 3] = v3;
+                }
+
+                for(k = i + 4; k < i + 8; k++){//the A's bottom right trans to B's bottom right
+                    v0 = A[k][j + 4];
+                    v1 = A[k][j + 5];
+                    v2 = A[k][j + 6];
+                    v3 = A[k][j + 7];
+
+                    B[j + 4][k] = v0;
+                    B[j + 5][k] = v1;
+                    B[j + 6][k] = v2;
+                    B[j + 7][k] = v3;
+                }
+            }
+        } 
+    }
+    else{//61x67 matrix divides to 17x17 blocks can satisfy the demands,about 1950 misses
+        int i, j;
+        int k, l;
+        for(i = 0; i < N; i += 17){
+            for(j = 0; j < M; j += 17){
+                for(k = i; k < N && k < i + 17; k++){
+                    for(l = j; l < M && l < j + 17; l++)
+                        B[l][k] = A[k][l];
+                }
+            }
+        }
+        
+    }
 }
 
 /* 
